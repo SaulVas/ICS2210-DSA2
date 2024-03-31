a209239bb4ba4aa06bb4411487590c454d66e9c7
SaulVas <saul.vassallo.22@um.edu.mt>
2024-03-31 22:58:20 +0200

    Inserted second array into avl tree

--- avl.py
+++ avl.py
@@ -122,3 +122,73 @@ class AVLTree:
             return self._rotate_left(node)
 
         return node
+
+    def insertion_steps_and_rotation(self, key):
+        """
+        Perform an insertion of a key into the AVL tree and return the number 
+        of steps taken and whether a rotation was performed or not.
+
+        Parameters:
+        - key: The key to be inserted into the AVL tree.
+
+        Returns:
+        A tuple containing the number of steps taken during the 
+        insertion process and 1 if a rotation occured or 0 otherwise.
+        """
+        self.root, steps, rotation = self._insert_steps_and_rotation(self.root, key)
+        return (steps, rotation)
+
+    def _insert_steps_and_rotation(self, node, key):
+        # Rec cases
+        if node is None:
+            return (AVLNode(key), 0, 0)
+        if key < node.key:
+            (node.left, steps, rotation) = self._insert_steps_and_rotation(node.left, key)
+        else:
+            (node.right, steps, rotation) = self._insert_steps_and_rotation(node.right, key)
+
+        # Adjust heights of nodes after insertion and check balancing condition
+        height_left = self._get_height(node.left)
+        height_right = self._get_height(node.right)
+        node.set_height(1 + max(height_left, height_right))
+        bal_factor = height_left - height_right
+
+        # Perform rotations if required
+        if bal_factor > 1:
+            # LL or LR
+            if key < node.left.key:
+                return (self._rotate_right(node), steps + 1, rotation + 1)
+
+            node.left = self._rotate_left(node.left)
+            return (self._rotate_right(node), steps + 1, rotation + 1)
+        if bal_factor < -1:
+            # RR or RL
+            if key >= node.right.key:
+                return (self._rotate_left(node), steps + 1, rotation + 1)
+
+            node.right = self._rotate_right(node.right)
+            return (self._rotate_left(node), steps + 1, rotation + 1)
+
+        return (node, steps + 1, rotation)
+
+    def get_leaves(self):
+        """
+        Returns the number of leaves in the AVL tree.
+
+        Returns:
+            int: The number of leaves in the AVL tree.
+        """
+        leaves = 0
+        return self._get_leaves(self.root, leaves)
+
+
+    def _get_leaves(self, node, count):
+        if not node:
+            return count
+        if node.left is None and node.right is None:
+            return count + 1
+
+        new_count = self._get_leaves(node.left, count)
+        return self._get_leaves(node.right, new_count)
+        
+        
--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 """
 
 from random import randint
+from statistics import mean, stdev, median
+
+from numpy import insert
 from avl import AVLTree
 
 def knuth_shuffle(array):
@@ -20,6 +23,7 @@ def knuth_shuffle(array):
         array[index], array[swap_index] = array[swap_index], array[index]
 
 if __name__ == "__main__":
+    # Insertion of First array
     integers = list(range(1, 5001))
     knuth_shuffle(integers)
 
@@ -27,3 +31,32 @@ if __name__ == "__main__":
     avl_tree = AVLTree()
     for number in integers:
         avl_tree.insert(number)
+
+
+    # Insertion of Second array
+    second_integers = [randint(1, 100000) for _ in range(1000)]
+
+    # AVL Tree section
+    insertion_steps = []
+    rotations = []
+    for num in second_integers:
+        steps, rotation = avl_tree.insertion_steps_and_rotation(num)
+        insertion_steps.append(steps)
+        rotations.append(rotation)
+
+    print("Insertion Steps Statistics:")
+    print(f"Minimum: {min(insertion_steps)}")
+    print(f"Maximum: {max(insertion_steps)}")
+    print(f"Mean: {mean(insertion_steps)}")
+    print(f"Standard Deviation: {stdev(insertion_steps)}")
+    print(f"Median: {median(insertion_steps)}\n")
+
+    print("Rotations Statistics:")
+    print(f"Minimum: {min(rotations)}")
+    print(f"Maximum: {max(rotations)}")
+    print(f"Mean: {mean(rotations)}")
+    print(f"Standard Deviation: {stdev(rotations)}")
+    print(f"Median: {median(rotations)}\n")
+
+    print(f"Tree Height: {avl_tree.root.height}")
+    print(f"Leaves: {avl_tree.get_leaves()}")
